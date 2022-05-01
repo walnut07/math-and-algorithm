934fe50d09d3664081ce44d172ff076fc4580a3d
kurumi <p20080mk@gm.tsuda.ac.jp>
2022-05-01 22:06:01 +0900

    Solved this problem for the first time

--- chapter-4-problems/4.2.1-1-Travel.py
+++ chapter-4-problems/4.2.1-1-Travel.py
@@ -8,16 +8,16 @@ for i in range(M):
     B[i] = num
 
 #cumulative sum
-CM = [ 0 ] * ( N )
-CM[0] = A[0]
+C = [ 0 ] * ( N ) #N = A + 1
+C[0] = 0
 for i in range(1, N):
-    CM[i] = CM[i - 1] + A[i - 1]
+    C[i] = C[i - 1] + A[i - 1]
 
 ans = 0
 for i in range(M - 1):
-    if B[i] < B[i + 1]:
-        ans += CM[ B[i + 1] - 1 ] - CM[ B[i] - 1 ]
+    if C[B[i] - 1] < C[B[i + 1] - 1]:
+        ans +=  ( C[B[i + 1] - 1] ) - ( C[B[i] - 1] )
     else:
-        ans += CM[B[i] - 1] - CM[B[i + 1] - 1]
+        ans +=  ( C[B[i] - 1] ) - ( C[B[i + 1] - 1])
 
 print(ans)
